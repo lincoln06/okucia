243cd6583d727c7926ca7f892c7f9f9714986943
MJL <maciekjl1993@gmail.com>
2021-11-14 09:37:34 +0100

    dodanie wszystkich szkieletów funkcji

--- okucia/Program.cs
+++ okucia/Program.cs
@@ -7,10 +7,28 @@ namespace okucia
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine("Program obliczy okucia potrzebne do wykonania poedyczego skrzydła okiennego o właściwościach zadanych przez użytkownika");
+            Console.WriteLine("Program obliczy okucia potrzebne do wykonania pojedyczego skrzydła okiennego o właściwościach zadanych przez użytkownika");
+            Console.ReadKey();
+            Console.WriteLine("Podaj typ skrzydła");
+            Console.WriteLine("1 - Uchylne\n2 - Rozwierne\n3 - Rozwierno - uchylne\n4 - Rozwierno - uchylne + rozwierne ze słupkiem ruchomym");
             Console.ReadKey();
 
-
+        }
+        static void Uchyl(int x,int y)
+        {
+            //Funkcja dla okna U
+        }
+        static void ru(int x, int y)
+        {
+            //funkcja dla okna RU
+        }
+        static void roz(int x, int y)
+        {
+            //funkcja dla okna r
+        }
+        static void ruch(int x, int y)
+        {
+            //funkcja dla ru+r na ruchomym
         }
     }
 }
