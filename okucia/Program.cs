48fbc3d390e4e9cc28b19d2efc333a2accd76e07
MJL <maciekjl1993@gmail.com>
2021-11-14 09:24:53 +0100

    surowy program

---
+++ okucia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace okucia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
